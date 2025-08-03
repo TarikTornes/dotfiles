c9f4ba9522cc707e7cf60f26a445548a4e90a0ef
TarikTornes <tarik.tornes03@gmail.com>
2025-08-03 23:00:20 +0200

    Lexplorer keymap

--- dot_config/nvim/lua/tariktornes/keymaps.lua
+++ dot_config/nvim/lua/tariktornes/keymaps.lua
@@ -1,32 +1,18 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- alias/variable for the keymapping
-local keymap = vim.api.nvim_set_keymap
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- noremap = true ensures that keymapping wont propagate
 -- silent = true disables that the keymappings will be shown in the cl when executed
 local opts = { noremap = true, silent = true }
 
-
-
-
-
-
-
 -- NEUTRAL --
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
-
-
-
-
-
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>l", "<cmd>Lex<CR>", { desc = "[L]explorer" })
 
 -- INSERT --
 
 -- Press jk fast to enter
-vim.keymap.set('i', 'jk', '<ESC>', opts)
+vim.keymap.set("i", "jk", "<ESC>", opts)
