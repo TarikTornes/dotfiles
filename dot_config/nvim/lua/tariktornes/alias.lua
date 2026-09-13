f0cdd554f5187a43819b161724ba44b5ece2ea96
TarikTornes <tarik.tornes03@gmail.com>
2026-09-13 15:02:19 +0200

    Remove old nvim config modules replaced by lazy.nvim structure

--- dot_config/nvim/lua/tariktornes/alias.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_create_user_command('Spell', 'setlocal spell spelllang=en_us', {})
-vim.api.nvim_create_user_command('Spellde', 'setlocal spell spelllang=de', {})
-vim.api.nvim_create_user_command('NoSpell', 'setlocal nospell', {})
